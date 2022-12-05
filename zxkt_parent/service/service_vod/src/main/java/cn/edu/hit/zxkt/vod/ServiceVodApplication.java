123d4668619ad0e9bb77d42f90f120bd0779a81a
Tsukinai <909436488@qq.com>
2022-12-05 13:35:23 +0800

    定义扫描路径cn.edu.hit 进行spring装配

--- zxkt_parent/service/service_vod/src/main/java/cn/edu/hit/zxkt/vod/ServiceVodApplication.java
+++ zxkt_parent/service/service_vod/src/main/java/cn/edu/hit/zxkt/vod/ServiceVodApplication.java
@@ -2,8 +2,10 @@ package cn.edu.hit.zxkt.vod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "cn.edu.hit")
 public class ServiceVodApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceVodApplication.class, args);
