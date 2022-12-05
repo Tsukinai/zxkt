505fee7e4ec4683ffff52e3beb353ca9be708b67
Tsukinai <909436488@qq.com>
2022-12-05 13:36:46 +0800

    增加api注解

--- zxkt_parent/service/service_vod/src/main/java/cn/edu/hit/zxkt/vod/controller/TeacherController.java
+++ zxkt_parent/service/service_vod/src/main/java/cn/edu/hit/zxkt/vod/controller/TeacherController.java
@@ -3,6 +3,8 @@ package cn.edu.hit.zxkt.vod.controller;
 
 import cn.edu.hit.zxkt.model.vod.Teacher;
 import cn.edu.hit.zxkt.vod.service.TeacherService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,7 @@ import java.util.List;
  * @author Tsukinai
  * @since 2022-12-04
  */
+@Api(tags = "讲师管理接口")
 @RestController
 @RequestMapping("/admin/vod/teacher")
 public class TeacherController {
@@ -26,17 +29,17 @@ public class TeacherController {
 
     //localhost:8301/admin/vod/teacher/findAll
     //1 查询所有讲师
+    @ApiOperation("查询所有讲师")
     @GetMapping("findAll")
     public List<Teacher> findAllTeacher() {
-        List<Teacher> list = teacherService.list();
-        return list;
+        return teacherService.list();
     }
 
     //2 逻辑删除讲师
+    @ApiOperation("逻辑删除讲师")
     @DeleteMapping("remove/{id}")
     public boolean removeTeacher(@PathVariable Long id) {
-        boolean isSuccess = teacherService.removeById(id);
-        return isSuccess;
+        return teacherService.removeById(id);
     }
 
 }
