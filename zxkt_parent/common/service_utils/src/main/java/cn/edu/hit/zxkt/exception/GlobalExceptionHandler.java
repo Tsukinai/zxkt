4c8274c04ee0b035b296c341c651c6fc71179f73
Tsukinai <909436488@qq.com>
2022-12-05 22:43:38 +0800

    特定异常处理和自定义异常处理

--- zxkt_parent/common/service_utils/src/main/java/cn/edu/hit/zxkt/exception/GlobalExceptionHandler.java
+++ zxkt_parent/common/service_utils/src/main/java/cn/edu/hit/zxkt/exception/GlobalExceptionHandler.java
@@ -14,4 +14,20 @@ public class GlobalExceptionHandler {
         e.printStackTrace();
         return Result.fail(null).message("执行全局异常处理");
     }
+
+    //特定异常处理
+    @ExceptionHandler(ArithmeticException.class)
+    @ResponseBody
+    public Result error(ArithmeticException e) {
+        e.printStackTrace();
+        return Result.fail(null).message("执行Arithmetic异常处理");
+    }
+
+    //自定义异常处理
+    @ExceptionHandler(ZxktException.class)
+    @ResponseBody
+    public Result error(ZxktException e) {
+        e.printStackTrace();
+        return Result.fail(null).code(e.getCode()).message(e.getMsg());
+    }
 }
