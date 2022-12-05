908d9526b747a8ea5bbf952099731e48e55a9c55
Tsukinai <909436488@qq.com>
2022-12-05 22:13:27 +0800

    实现全局异常处理

--- zxkt_parent/common/service_utils/src/main/java/cn/edu/hit/zxkt/exception/GlobalExceptionHandler.java
+++ zxkt_parent/common/service_utils/src/main/java/cn/edu/hit/zxkt/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package cn.edu.hit.zxkt.exception;
 
 import cn.edu.hit.zxkt.result.Result;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
