8f362e7ce44eb9e5e8bf2f4be3e96d60dd5ab752
shizuku <ziluolin747@gmail.com>
2022-12-05 19:48:27 +0800

    定义调用接口

---
+++ zxkt_front/vue-admin-template/src/api/vod/teacher.js
@@ -0,0 +1,14 @@
+import request from '@/utils/request'
+
+const api_name = '/admin/vod/teacher'
+export default {
+    //讲师查询分页
+    pageList(current, limit, searchObj) {
+        return request({
+            url: `${api_name}/findQueryPage/${current}/${limit}`,
+            method: 'post',
+            //使用json对象传递
+            data:searchObj
+        })
+    }
+}
