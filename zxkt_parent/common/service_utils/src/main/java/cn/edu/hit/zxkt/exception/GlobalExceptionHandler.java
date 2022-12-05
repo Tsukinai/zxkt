05f04b6c44b32a5d6adaf7acda70b2255f7c9a38
Tsukinai <909436488@qq.com>
2022-12-05 22:11:10 +0800

    实现全局异常处理

---
+++ zxkt_parent/common/service_utils/src/main/java/cn/edu/hit/zxkt/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package cn.edu.hit.zxkt.exception;
+
+import cn.edu.hit.zxkt.result.Result;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice //aop
+public class GlobalExceptionHandler {
+    //全局异常处理
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e) {
+        e.printStackTrace();
+        return Result.fail(null).message("执行全局异常处理");
+    }
+}
