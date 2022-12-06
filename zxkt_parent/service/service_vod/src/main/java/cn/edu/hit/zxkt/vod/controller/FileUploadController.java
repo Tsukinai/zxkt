93565c4db62b89cce4fa6987c87c1824f8c5d817
shizuku <ziluolin747@gmail.com>
2022-12-06 11:29:53 +0800

    前端实现讲师图片上传

--- zxkt_parent/service/service_vod/src/main/java/cn/edu/hit/zxkt/vod/controller/FileUploadController.java
+++ zxkt_parent/service/service_vod/src/main/java/cn/edu/hit/zxkt/vod/controller/FileUploadController.java
@@ -4,11 +4,9 @@ import cn.edu.hit.zxkt.result.Result;
 import cn.edu.hit.zxkt.vod.service.FileService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 @Api(tags = "文件上传接口")
@@ -22,8 +20,9 @@ public class FileUploadController {
 
     @ApiOperation("文件上传")
     @PostMapping("upload")
-    public Result uploadFile(MultipartFile file){
+    public Result uploadFile(@ApiParam(name = "file", value = "文件", required = true)
+                                 @RequestParam("file") MultipartFile file){
         String url=fileService.upload(file);
-        return Result.ok(null);
+        return Result.ok(url);
     }
 }
