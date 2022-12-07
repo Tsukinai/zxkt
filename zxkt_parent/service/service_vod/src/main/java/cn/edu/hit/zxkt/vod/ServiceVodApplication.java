8c92e4d484bcbe3d043eab138af8cf5e40b73f3f
Tsukinai <909436488@qq.com>
2022-12-07 20:10:12 +0800

    将服务注册到nacos

--- zxkt_parent/service/service_vod/src/main/java/cn/edu/hit/zxkt/vod/ServiceVodApplication.java
+++ zxkt_parent/service/service_vod/src/main/java/cn/edu/hit/zxkt/vod/ServiceVodApplication.java
@@ -2,10 +2,12 @@ package cn.edu.hit.zxkt.vod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "cn.edu.hit")
+@EnableDiscoveryClient
 public class ServiceVodApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceVodApplication.class, args);
