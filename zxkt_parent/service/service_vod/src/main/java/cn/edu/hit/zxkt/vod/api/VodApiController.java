6bc2a6ee52dece6a755dd584b85d0d3a42e624ad
Jiuyao Qu <2668136485@qq.com>
2022-12-09 15:14:57 +0800

    公众号点播课程播放接口 （腾讯云的appId还没加）

---
+++ zxkt_parent/service/service_vod/src/main/java/cn/edu/hit/zxkt/vod/api/VodApiController.java
@@ -0,0 +1,30 @@
+package cn.edu.hit.zxkt.vod.api;
+
+import cn.edu.hit.zxkt.result.Result;
+import cn.edu.hit.zxkt.vod.service.VodService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Api(tags = "腾讯视频点播")
+@RestController
+@RequestMapping("/api/vod")
+public class VodApiController {
+    @Autowired
+    private VodService vodService;
+
+    @GetMapping("getPlayAuth/{courseId}/{videoId}")
+    public Result getPlayAuth(@ApiParam(value = "课程id",required = true)
+                              @PathVariable Long courseId,
+                              @ApiParam(value = "小节id",required = true)
+                              @PathVariable Long videoId){
+        Map<String,Object> map = vodService.getPlayAuth(courseId,videoId);
+        return Result.ok(map);
+    }
+}
--- zxkt_parent/service/service_vod/src/main/java/cn/edu/hit/zxkt/vod/service/VodService.java
+++ zxkt_parent/service/service_vod/src/main/java/cn/edu/hit/zxkt/vod/service/VodService.java
@@ -1,7 +1,11 @@
 package cn.edu.hit.zxkt.vod.service;
 
+import java.util.Map;
+
 public interface VodService {
     String updateVideo(String url);
 
     void removeVideo(String fileId);
+
+    Map<String, Object> getPlayAuth(Long courseId, Long videoId);
 }
